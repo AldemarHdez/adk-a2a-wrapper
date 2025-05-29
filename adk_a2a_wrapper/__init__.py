3b6d36c02df3ba0c943b4231fc2d45e82d577571
aldemar <143541998+AldemarHdez@users.noreply.github.com>
2025-05-29 15:54:11 +0200

    Export SkillDefinition from models

--- adk_a2a_wrapper/__init__.py
+++ adk_a2a_wrapper/__init__.py
@@ -1,4 +1,4 @@
 from .wrapper import create_a2a_agent, A2AAgentServer
-from .models import AgentRequest, AgentResponse
+from .models import AgentRequest, AgentResponse, SkillDefinition
 
-__all__ = ['create_a2a_agent', 'A2AAgentServer', 'AgentRequest', 'AgentResponse']
+__all__ = ['create_a2a_agent', 'A2AAgentServer', 'AgentRequest', 'AgentResponse', 'SkillDefinition']
