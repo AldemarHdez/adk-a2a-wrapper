d853fb5df9cfc0f449a1d7d69f31eeba3ef5791a
aldemar <143541998+AldemarHdez@users.noreply.github.com>
2025-05-29 16:10:17 +0200

    Export CollaborativeAgent class

--- adk_a2a_wrapper/__init__.py
+++ adk_a2a_wrapper/__init__.py
@@ -1,4 +1,12 @@
 from .wrapper import create_a2a_agent, A2AAgentServer
 from .models import AgentRequest, AgentResponse, SkillDefinition
+from .base_agent import CollaborativeAgent
 
-__all__ = ['create_a2a_agent', 'A2AAgentServer', 'AgentRequest', 'AgentResponse', 'SkillDefinition']
+__all__ = [
+    'create_a2a_agent', 
+    'A2AAgentServer', 
+    'AgentRequest', 
+    'AgentResponse', 
+    'SkillDefinition',
+    'CollaborativeAgent'
+]
